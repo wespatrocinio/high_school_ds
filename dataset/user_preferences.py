48c8cee76776170c8034747fbcb042f72c575220
Weslley Souza Patrocinio <wespatrocinio@gmail.com>
2015-08-11 23:08:37 -0300

    Bootstraping recommender

---
+++ dataset/user_preferences.py
@@ -0,0 +1,206 @@
+ALBUNS = [
+    "Foo Fighters",
+    "The Colour and the shape",
+    "There is nothing left to lose",
+    "One by one",
+    "In your honor",
+    "Echoes, silence, patience and grace",
+    "Wasting light",
+    "Sonic highways"
+]
+
+USERS = [
+    "John",
+    "Peter",
+    "Rose",
+    "Mary Jane",
+    "Augustus",
+    "Shirley",
+    "Hildebrando",
+    "Phillip III",
+    "Toto"
+]
+
+RATINGS_BY_USER = {
+    "John": {
+        "Foo Fighters": 0,
+        "The Colour and the shape": 0,
+        "There is nothing left to lose": 0,
+        "One by one": 0,
+        "In your honor": 0,
+        "Echoes, silence, patience and grace": 0,
+        "Wasting light": 0,
+        "Sonic highways": 0
+    },
+    "Peter": {
+        "Foo Fighters": 0,
+        "The Colour and the shape": 0,
+        "There is nothing left to lose": 0,
+        "One by one": 0,
+        "In your honor": 0,
+        "Echoes, silence, patience and grace": 0,
+        "Wasting light": 0,
+        "Sonic highways": 0
+    },
+    "Rose": {
+        "Foo Fighters": 0,
+        "The Colour and the shape": 0,
+        "There is nothing left to lose": 0,
+        "One by one": 0,
+        "In your honor": 0,
+        "Echoes, silence, patience and grace": 0,
+        "Wasting light": 0,
+        "Sonic highways": 0
+    },
+    "Mary Jane": {
+        "Foo Fighters": 0,
+        "The Colour and the shape": 0,
+        "There is nothing left to lose": 0,
+        "One by one": 0,
+        "In your honor": 0,
+        "Echoes, silence, patience and grace": 0,
+        "Wasting light": 0,
+        "Sonic highways": 0
+    },
+    "Augustus": {
+        "Foo Fighters": 0,
+        "The Colour and the shape": 0,
+        "There is nothing left to lose": 0,
+        "One by one": 0,
+        "In your honor": 0,
+        "Echoes, silence, patience and grace": 0,
+        "Wasting light": 0,
+        "Sonic highways": 0
+    },
+    "Shirley": {
+        "Foo Fighters": 0,
+        "The Colour and the shape": 0,
+        "There is nothing left to lose": 0,
+        "One by one": 0,
+        "In your honor": 0,
+        "Echoes, silence, patience and grace": 0,
+        "Wasting light": 0,
+        "Sonic highways": 0
+    },
+    "Hildebrando": {
+        "Foo Fighters": 0,
+        "The Colour and the shape": 0,
+        "There is nothing left to lose": 0,
+        "One by one": 0,
+        "In your honor": 0,
+        "Echoes, silence, patience and grace": 0,
+        "Wasting light": 0,
+        "Sonic highways": 0
+    },
+    "Phillip III": {
+        "Foo Fighters": 0,
+        "The Colour and the shape": 0,
+        "There is nothing left to lose": 0,
+        "One by one": 0,
+        "In your honor": 0,
+        "Echoes, silence, patience and grace": 0,
+        "Wasting light": 0,
+        "Sonic highways": 0
+    },
+    "Toto": {
+        "Foo Fighters": 0,
+        "The Colour and the shape": 0,
+        "There is nothing left to lose": 0,
+        "One by one": 0,
+        "In your honor": 0,
+        "Echoes, silence, patience and grace": 0,
+        "Wasting light": 0,
+        "Sonic highways": 0
+    }
+}
+
+RATINGS_BY_ALBUM = {
+    "Foo Fighters": {
+        "John": 0,
+        "Peter": 0,
+        "Rose": 0,
+        "Mary Jane": 0,
+        "Augustus": 0,
+        "Shirley": 0,
+        "Hildebrando": 0,
+        "Phillip III": 0,
+        "Toto": 0
+    },
+    "The Colour and the shape": {
+        "John": 0,
+        "Peter": 0,
+        "Rose": 0,
+        "Mary Jane": 0,
+        "Augustus": 0,
+        "Shirley": 0,
+        "Hildebrando": 0,
+        "Phillip III": 0,
+        "Toto": 0
+    },
+    "There is nothing left to lose": {
+        "John": 0,
+        "Peter": 0,
+        "Rose": 0,
+        "Mary Jane": 0,
+        "Augustus": 0,
+        "Shirley": 0,
+        "Hildebrando": 0,
+        "Phillip III": 0,
+        "Toto": 0
+    },
+    "One by one": {
+        "John": 0,
+        "Peter": 0,
+        "Rose": 0,
+        "Mary Jane": 0,
+        "Augustus": 0,
+        "Shirley": 0,
+        "Hildebrando": 0,
+        "Phillip III": 0,
+        "Toto": 0
+    },
+    "In your honor": {
+        "John": 0,
+        "Peter": 0,
+        "Rose": 0,
+        "Mary Jane": 0,
+        "Augustus": 0,
+        "Shirley": 0,
+        "Hildebrando": 0,
+        "Phillip III": 0,
+        "Toto": 0
+    },
+    "Echoes, silence, patience and grace": {
+        "John": 0,
+        "Peter": 0,
+        "Rose": 0,
+        "Mary Jane": 0,
+        "Augustus": 0,
+        "Shirley": 0,
+        "Hildebrando": 0,
+        "Phillip III": 0,
+        "Toto": 0
+    },
+    "Wasting light": {
+        "John": 0,
+        "Peter": 0,
+        "Rose": 0,
+        "Mary Jane": 0,
+        "Augustus": 0,
+        "Shirley": 0,
+        "Hildebrando": 0,
+        "Phillip III": 0,
+        "Toto": 0
+    },
+    "Sonic highways": {
+        "John": 0,
+        "Peter": 0,
+        "Rose": 0,
+        "Mary Jane": 0,
+        "Augustus": 0,
+        "Shirley": 0,
+        "Hildebrando": 0,
+        "Phillip III": 0,
+        "Toto": 0
+    }
+}
